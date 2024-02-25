f1754f10d5603fcbd7b819e3bd4a93afc1071e90
abhilash <abhilash.chaurasiya123@gmail.com>
2024-02-25 11:43:52 +0530

    create custom api

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from api.models import Company,Employee
 
 # Register your models here.
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name','location','type',)
+    #search_fields = ('name',)
+    list_editable = ('location',)
+    list_filter = ('type',)
+
+admin.site.register(Company,CompanyAdmin)
+admin.site.register(Employee)
